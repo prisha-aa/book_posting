f1cc887b6b37fab3ea8eceb9e24557555bcd42c7
Prisha <prishabhasin14@gmail.com>
2025-07-03 15:05:40 +0400

    plug everything together

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { BookService } from "./core/services/BookService";
+import { InMemoryBookRepository } from "./adapters/out/InMemoryBookRepository";
+import { createBookApi } from "./adapters/in/BookApiController";
+
+const app = express();
+app.use(express.json());
+
+
+const repo = new InMemoryBookRepository();
+const service = new BookService(repo);
+const bookApi = createBookApi(service);
+
+app.use("/books", bookApi);
+
+app.listen(3000, () => {
+  console.log("Book app running at http://localhost:3000");
+});
