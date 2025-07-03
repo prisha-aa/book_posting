13354cf5f3e11f550c3b7a2282378ba67cc14440
Prisha <prishabhasin14@gmail.com>
2025-07-03 14:04:46 +0400

    core logic done

--- src/core/services/BookService.ts
+++ src/core/services/BookService.ts
@@ -1,2 +1,29 @@
 import { Book } from "../../types/Book";
 import { BookServicePort } from "../ports/BookServicePort";
+import { BookRepositoryPort } from "../ports/BookRepositoryPort";
+
+export class BookService implements BookServicePort {
+    constructor(private repo: BookRepositoryPort) {}
+
+    createBook(data: Omit<Book, "id">): Book {
+        const book = {id:uuidv4(), ...data};
+        this.repo.save(book);
+        return book;
+    }
+
+    updateBook(id: string, data: Partial<Book>): void {
+        this.repo.update(id, data);
+    }
+
+    deleteBook(id:string){
+        this.repo.delete(id);
+    }
+
+    getBook(id:string){
+        return this.repo.get(id);   
+    }
+
+    listBooks(){
+        return this.repo.list() 
+    }
+}
