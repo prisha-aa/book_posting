81fd94421bcc144f3184722109a79f6bd17ca769
Prisha <prishabhasin14@gmail.com>
2025-07-05 16:42:47 +0400

    add auth service port for registering, login and token validation

--- src/core/ports/AuthServicePort.ts
+++ src/core/ports/AuthServicePort.ts
@@ -1,2 +1,8 @@
+import { User } from "../../types/User";
+
+
 export interface AuthServicePort {
     registerUser(username: string, password: string): Promise<User>;
+    loginUser(username: string, password: string): Promise<User>;
+    validateToken(token: string): Promise<User | null>;
+}
